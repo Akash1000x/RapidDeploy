c96d2b7f3aecfafc9bbe5b32a365670219ae2dd9
Akash kumar kumawat <kumawatap63@gmail.com>
2024-06-10 22:00:47 +0530

    Add redis

--- build-server/script.js
+++ build-server/script.js
@@ -3,6 +3,10 @@ const path = require("path");
 const fs = require("fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const mime = require("mime-types");
+const Redis = require("ioredis");
+
+//TODO: add reids url
+const publisher = new Redis(" redis url");
 
 //TODO: add credentials here
 const s3Client = new S3Client({
@@ -15,6 +19,10 @@ const s3Client = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID;
 
+function publishLOg(log) {
+  publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }));
+}
+
 async function init() {
   console.log("Executing script.js");
   const outDirPath = path.join(__dirname, "output");
@@ -23,16 +31,22 @@ async function init() {
 
   p.stdout.on("error", function (data) {
     console.log("Error", data.toString());
+    publishLOg(`error: ${data.toString()}`);
   });
 
   p.on("close", async function () {
     console.log("Build Complete");
+    publishLOg("Build Complete");
     const distFolderPath = path.join(__dirname, "output", "dist");
     const distFolderContent = fs.readdirSync(distFolderPath, { recursive: true });
 
+    publishLOg("Starting to upload");
     for (const file of distFolderContent) {
       const filePath = path.join(distFolderPath, file);
       if (fs.lstatSync(filePath).isDirectory()) continue;
+
+      console.log("uploadin", filePath);
+      publishLOg(`uploading ${file}`);
 
       const command = new PutObjectCommand({
         Bucket: "",
@@ -40,8 +54,13 @@ async function init() {
         Body: fs.createReadStream(filePath),
         ContentType: mime.lookup(filePath),
       });
-      await s3Client.send();
+      await s3Client.send(command);
+      publishLOg(`uploaded ${file}`);
+      publishLOg("uploaded", filePath);
     }
+    publishLOg("Done");
     console.log("Done...");
   });
 }
+
+init();
