7ae3fd231ea378d970f423717a7ac3555c1634f7
Akash kumar kumawat <kumawatap63@gmail.com>
2024-06-19 17:43:34 +0530

    Add kafka & remove redis

--- api-server/index.js
+++ api-server/index.js
@@ -2,18 +2,43 @@ const express = require("express");
 const { generateSlug } = require("random-word-slugs");
 const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs");
 const { Server } = require("socket.io");
-const Redis = require("ioredis");
 const { prismaClient } = require("./db");
 const { z } = require("zod");
+const cors = require("cors");
+const { createClient } = require("@clickhouse/client");
+const { Kafka } = require("kafkajs");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
 const PORT = 9000;
-
-//TODO: add reids url
-const subscriber = new Redis(" redis url");
+app.use(cors());
 
 const io = new Server({ cors: "*" });
+
+//connect to kafka
+const kafka = new Kafka({
+  clientId: `api-server`,
+  brokers: [],
+  ssl: {
+    ca: [fs.readFileSync(path.join(__dirname, "kafka.pem"), "utf-8")],
+  },
+  sasl: {
+    username: "",
+    password: "",
+    mechanism: "plain",
+  },
+});
+
+const consumer = kafka.consumer({ groupId: "api-server-logs-consumer" });
+
+const client = createClient({
+  host: "",
+  database: "",
+  username: "",
+  password: "",
+});
 
 io.on("connection", (socket) => {
   socket.on("subscribe", (channel) => {
@@ -107,17 +132,55 @@ app.post("/deploy", async (req, res) => {
 
   await ecsClient.send(command);
 
-  return res.json({ status: "queued", data: { projectSlug, url: `http://${projectSlug}.localhost:8000` } });
+  return res.json({ status: "queued", data: { deploymentId: deployment.id } });
 });
 
-async function initRedisSubscribe() {
-  console.log("Subscribed to log...");
-  subscriber.psubscribe("logs:*");
-  subscriber.on("pmessage", (pattern, channel, message) => {
-    io.to(channel).emit("message", message);
+app.get("/logs/:id", async (req, res) => {
+  const id = req.params.id;
+  const logs = await client.query({
+    query: `SELECT event_id, deployment_id, log, timestamp from log_events where deployment_id = {deployment_id:String}`,
+    query_params: {
+      deployment_id: id,
+    },
+    format: "JSONEachRow",
+  });
+
+  const rawLogs = await logs.json();
+
+  return res.json({ logs: rawLogs });
+});
+
+async function initkafkaConsumer() {
+  await consumer.connect();
+  await consumer.subscribe({ topics: ["container-logs"], fromBeginning: true });
+
+  await consumer.run({
+    eachBatch: async function ({ batch, heartbeat, commitOffsetsIfNecessary, resolveOffset }) {
+      const messages = batch.messages;
+      console.log(`Recv. ${messages.length} messages..`);
+      for (const message of messages) {
+        if (!message.value) continue;
+        const stringMessage = message.value.toString();
+        const { PROJECT_ID, DEPLOYEMENT_ID, log } = JSON.parse(stringMessage);
+        console.log({ log, DEPLOYEMENT_ID });
+        try {
+          const { query_id } = await client.insert({
+            table: "log_events",
+            values: [{ event_id: uuidv4(), deployment_id: DEPLOYEMENT_ID, log }],
+            format: "JSONEachRow",
+          });
+          console.log(query_id);
+          resolveOffset(message.offset);
+          await commitOffsetsIfNecessary(message.offset);
+          await heartbeat();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    },
   });
 }
 
-initRedisSubscribe();
+initkafkaConsumer();
 
 app.listen(PORT, () => console.log(`API Server Running.. ${PORT}`));
