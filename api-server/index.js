572ae8945edc62fea865bb25aba867c1f6b6da0c
Akash kumar kumawat <kumawatap63@gmail.com>
2024-06-27 00:14:24 +0530

    final update

--- api-server/index.js
+++ api-server/index.js
@@ -2,43 +2,20 @@ const express = require("express");
 const { generateSlug } = require("random-word-slugs");
 const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs");
 const { Server } = require("socket.io");
-const { prismaClient } = require("./db");
-const { z } = require("zod");
+const Redis = require("ioredis");
+const dotenv = require("dotenv");
 const cors = require("cors");
-const { createClient } = require("@clickhouse/client");
-const { Kafka } = require("kafkajs");
-const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
 
+dotenv.config();
 const app = express();
-app.use(express.json());
 const PORT = 9000;
-app.use(cors());
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+const subscriber = new Redis(process.env.REDIS_URL);
 
 const io = new Server({ cors: "*" });
-
-//connect to kafka
-const kafka = new Kafka({
-  clientId: `api-server`,
-  brokers: [],
-  ssl: {
-    ca: [fs.readFileSync(path.join(__dirname, "kafka.pem"), "utf-8")],
-  },
-  sasl: {
-    username: "",
-    password: "",
-    mechanism: "plain",
-  },
-});
-
-const consumer = kafka.consumer({ groupId: "api-server-logs-consumer" });
-
-const client = createClient({
-  host: "",
-  database: "",
-  username: "",
-  password: "",
-});
 
 io.on("connection", (socket) => {
   socket.on("subscribe", (channel) => {
@@ -47,63 +24,28 @@ io.on("connection", (socket) => {
   });
 });
 
-io.listen(9001, () => console.log("Socket Server 9001"));
+io.listen(9002, () => console.log("Socket Server 9002"));
 
 const ecsClient = new ECSClient({
-  region: "",
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: "",
-    secretAccessKey: "",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
 const config = {
-  CLUSTER: "",
-  TASK: "",
+  CLUSTER: process.env.AWS_ECS_CLUSTER,
+  TASK: process.env.AWS_ECS_TASK_DEFINITION,
 };
 
+app.use(express.json());
+
 app.post("/project", async (req, res) => {
-  const schema = z.object({
-    name: z.string(),
-    gitURL: z.string(),
-  });
+  const { gitURL, slug } = req.body;
+  const projectSlug = slug ? slug : generateSlug();
 
-  const safeParse = schema.safeParse(req.body);
-
-  if (safeParse.error) return res.status(400).json({ error: safeParse.error });
-
-  const { name, gitURL } = safeParse.data;
-
-  const project = await prismaClient.project.create({
-    data: {
-      name,
-      gitURL,
-      subDomain: generateSlug(),
-    },
-  });
-
-  return res.json({ status: "success", data: project });
-});
-
-app.post("/deploy", async (req, res) => {
-  const { projectId } = req.body;
-
-  const project = await prismaClient.project.findUnique({
-    where: {
-      id: projectId,
-    },
-  });
-
-  if (!project) return res.status(404).json({ error: "Project not found" });
-
-  const deployement = await prismaClient.deployement.create({
-    data: {
-      project: { connect: { id: projectId } },
-      status: "QUEUED",
-    },
-  });
-
-  //Spin the container
+  // Spin the container
   const command = new RunTaskCommand({
     cluster: config.CLUSTER,
     taskDefinition: config.TASK,
@@ -112,8 +54,9 @@ app.post("/deploy", async (req, res) => {
     networkConfiguration: {
       awsvpcConfiguration: {
         assignPublicIp: "ENABLED",
-        subnets: ["", "", ""], //TODO: add subnets form aws
-        securityGroups: [""],
+        //add subnet and security group here from your aws-ECS
+        subnets: ["subnet-...", "subnet-...", "subnet-..."],
+        securityGroups: ["sg-...."],
       },
     },
     overrides: {
@@ -121,9 +64,13 @@ app.post("/deploy", async (req, res) => {
         {
           name: "builder-image",
           environment: [
-            { name: "GIT_REPOSITORY__URL", value: project.gitURL },
-            { name: "PROJECT_ID", value: projectId },
-            { id: "DEPLOYMENT_ID", value: deployement.id },
+            { name: "GIT_REPOSITORY__URL", value: gitURL },
+            { name: "PROJECT_ID", value: projectSlug },
+            { name: "REDIS_URL", value: process.env.REDIS_URL },
+            { name: "AWS_BUCKET_NAME", value: process.env.AWS_BUCKET_NAME },
+            { name: "AWS_REGION", value: process.env.AWS_REGION },
+            { name: "AWS_ACCESS_KEY_ID", value: process.env.AWS_ACCESS_KEY_ID },
+            { name: "AWS_SECRET_ACCESS_KEY", value: process.env.AWS_SECRET_ACCESS_KEY },
           ],
         },
       ],
@@ -132,55 +79,17 @@ app.post("/deploy", async (req, res) => {
 
   await ecsClient.send(command);
 
-  return res.json({ status: "queued", data: { deploymentId: deployment.id } });
+  return res.json({ status: "queued", data: { projectid: projectSlug } });
 });
 
-app.get("/logs/:id", async (req, res) => {
-  const id = req.params.id;
-  const logs = await client.query({
-    query: `SELECT event_id, deployment_id, log, timestamp from log_events where deployment_id = {deployment_id:String}`,
-    query_params: {
-      deployment_id: id,
-    },
-    format: "JSONEachRow",
-  });
-
-  const rawLogs = await logs.json();
-
-  return res.json({ logs: rawLogs });
-});
-
-async function initkafkaConsumer() {
-  await consumer.connect();
-  await consumer.subscribe({ topics: ["container-logs"], fromBeginning: true });
-
-  await consumer.run({
-    eachBatch: async function ({ batch, heartbeat, commitOffsetsIfNecessary, resolveOffset }) {
-      const messages = batch.messages;
-      console.log(`Recv. ${messages.length} messages..`);
-      for (const message of messages) {
-        if (!message.value) continue;
-        const stringMessage = message.value.toString();
-        const { PROJECT_ID, DEPLOYEMENT_ID, log } = JSON.parse(stringMessage);
-        console.log({ log, DEPLOYEMENT_ID });
-        try {
-          const { query_id } = await client.insert({
-            table: "log_events",
-            values: [{ event_id: uuidv4(), deployment_id: DEPLOYEMENT_ID, log }],
-            format: "JSONEachRow",
-          });
-          console.log(query_id);
-          resolveOffset(message.offset);
-          await commitOffsetsIfNecessary(message.offset);
-          await heartbeat();
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    },
+async function initRedisSubscribe() {
+  console.log("Subscribed to logs....");
+  subscriber.psubscribe("logs:*");
+  subscriber.on("pmessage", (pattern, channel, message) => {
+    io.to(channel).emit("message", message);
   });
 }
 
-initkafkaConsumer();
+initRedisSubscribe();
 
-app.listen(PORT, () => console.log(`API Server Running.. ${PORT}`));
+app.listen(PORT, () => console.log(`API Server Running..${PORT}`));
