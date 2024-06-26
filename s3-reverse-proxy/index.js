03ce9ae24cc58ae42ade1a559711d8df1dd4710d
Akash kumar kumawat <kumawatap63@gmail.com>
2024-06-26 22:31:18 +0530

    fix reverse proxy

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const httpProxy = require("http-proxy");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
 const PORT = 8000;
 
-//TODO: change to own the bucket url
-const BASE_PATH = "https://{...}.s3.ap-south-1.amazonaws.com/__outputs";
+// S3 Bucket Base Path
+const BASE_PATH = process.env.S3_BUCKET_BASE_PATH;
 
 const proxy = httpProxy.createProxy();
 
@@ -12,16 +15,14 @@ app.use((req, res) => {
   const hostname = req.hostname;
   const subdomain = hostname.split(".")[0];
 
-  const resolveTo = `${BASE_PATH}/${subdomain}`;
+  const resolvesTo = `${BASE_PATH}/${subdomain}`;
 
-  return proxy.web(req, res, { target: resolveTo, changeOrigin: true });
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
 });
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
   const url = req.url;
-  if (url === "/") {
-    proxyReq.path += "/index.html";
-  }
+  if (url === "/") proxyReq.path += "index.html";
 });
 
-app.listen(PORT, () => console.log(`Reverse Proxy Running.. ${PORT}`));
+app.listen(PORT, () => console.log(`Reverse Proxy Running..${PORT}`));
